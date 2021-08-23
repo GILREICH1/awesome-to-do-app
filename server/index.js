0636f2b29eb0f5c5511d53a01aba4bdf9f20bdce
gilreich1 <gilreich12@gmail.com>
2021-08-23 18:37:03 +0100

    chore: cleanup .env usage

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const app = require("./server");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`app available at http://localhost:${PORT}`);
