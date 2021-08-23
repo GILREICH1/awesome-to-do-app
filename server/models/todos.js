be2b475b209427e5bc41f32f2f1215b78de7d638
gilreich1 <gilreich12@gmail.com>
2021-08-23 17:54:07 +0100

    feat: database input validation

--- server/models/todos.js
+++ server/models/todos.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema({
-  content: String,
-  priority: String,
+  content: { type: String, required: true },
+  priority: { type: String, required: false },
   categoriesIds: [String],
-  completed: {
-    type: Boolean,
-    default: false,
-  },
+  completed: { type: Boolean, default: false },
 });
 
 const TODO = mongoose.model("todo", todoSchema);
