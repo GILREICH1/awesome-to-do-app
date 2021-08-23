d539d1c2d29b1c7fa939af52c22c4892fbb3c86c
gilreich1 <gilreich12@gmail.com>
2021-08-23 18:26:42 +0100

    feat: more data validation in schema

--- server/models/todos.js
+++ server/models/todos.js
@@ -1,10 +1,24 @@
 const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema({
-  content: { type: String, required: true },
-  priority: { type: String, required: false },
-  categoriesIds: [String],
-  completed: { type: Boolean, default: false },
+  content: {
+    type: String,
+    required: [true, "To Do content required"],
+  },
+  priority: {
+    type: String,
+    required: false,
+    enum: ["High", "Medium", "Low"],
+  },
+  categoriesIds: {
+    type: [String],
+    require: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
 });
 
 const TODO = mongoose.model("todo", todoSchema);
